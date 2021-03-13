86665af25523ce22f4fb0a60ac03561f1832bb9d
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-03-13 12:12:18 +0300

    pull request notes fixed

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,16 @@
 import styles from "./style.module.css";
 
-const Layout = (props) => {
+const Layout = ({ urlBg, colorBg, descr, title }) => {
   return (
-    <section className={styles.root} style={{ background: `${props.urlBg ? `url(${props.urlBg})` : props.colorBg}` }}>
+    <section className={styles.root} style={{ background: `${urlBg ? `url(${urlBg})` : colorBg ? colorBg : "#e3e3e3"}` }}>
       <div className={styles.wrapper}>
         <article>
           <div className={styles.title}>
-            <h3>{props.title}</h3>
+            <h3>{title ? title : null}</h3>
             <span className={styles.separator} />
           </div>
           <div className={(styles.desc, styles.full)}>
-            <p>{props.descr}</p>
+            <p>{descr ? descr : null}</p>
           </div>
         </article>
       </div>
