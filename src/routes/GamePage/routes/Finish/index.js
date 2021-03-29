3903fd083283a80a16ef06a05051af26948256bc
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2021-03-29 21:00:11 +0300

    checked card hoisting and pushing to DB

--- src/routes/GamePage/routes/Finish/index.js
+++ src/routes/GamePage/routes/Finish/index.js
@@ -10,24 +10,23 @@ import PokemonCard from "../../../../components/PokemonCard";
 import styles from "./style.module.css";
 
 const FinishPage = () => {
-  const [chosenCard, setChosenCard] = useState(null);
+  const [selectedPokemon, setSelectedPokemon] = useState({});
   const pokemonContext = useContext(PokemonContext);
   const firebase = useContext(FireBaseContext);
   const history = useHistory();
 
   const handleCardClick = (key) => {
-    const pokemonAddToDatabase = { ...pokemonContext.player2Pokemons[key] };
-    // setChosenCard((prevState) => {
-    //   return {
-    //     ...prevState,
-    //     selected: !prevState.selected,
-    //   };
-    // });
-    console.log(pokemonAddToDatabase);
-    //firebase.addPokemon(pokemonAddToDatabase);
+    const pokemonToAdd = { ...pokemonContext.player2Pokemons[key] };
+    //setSelectedPokemon(pokemonToAdd);
+    setSelectedPokemon({
+      ...pokemonToAdd,
+      isSelected: !pokemonToAdd.isSelected,
+    });
+    console.log(selectedPokemon);
   };
 
-  const handleClick = () => {
+  const handleEndGame = () => {
+    selectedPokemon && firebase.addPokemon(selectedPokemon);
     pokemonContext.onClearContext();
     history.push("/game");
   };
@@ -41,12 +40,12 @@ const FinishPage = () => {
           ))}
         </div>
         <div>
-          <button onClick={handleClick}>End Game</button>
+          <button onClick={handleEndGame}>End Game</button>
         </div>
         <div>
           <div className={styles.flex}>
             {Object.entries(pokemonContext.player2Pokemons).map(([key, card]) => (
-              <PokemonCard key={key} keyId={key} name={card.name} img={card.img} id={card.id} type={card.type} values={card.values} active={true} className={styles.card} selected={false} onClickCard={handleCardClick()} />
+              <PokemonCard key={key} keyId={key} name={card.name} img={card.img} id={card.id} type={card.type} values={card.values} active={true} className={styles.card} isSelected={selectedPokemon.id ? selectedPokemon.id === card.id : false} onClickCard={handleCardClick} />
             ))}
           </div>
         </div>
